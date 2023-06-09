198f128ae911b640421fda3771d9d2efefd4c345
Andrew Goode <akgoode@gmail.com>
2023-06-09 16:48:44 -0500

    much more robust and graceful error handling

--- cli_rpn/command_line_calculator.py
+++ cli_rpn/command_line_calculator.py
@@ -1,25 +1,39 @@
 from RPNCalculator import Calculator
 
 
+class Shutdown(Exception):
+    pass
+
+
 class CommandLineCalculator:
     def __init__(self):
         self.c = Calculator()
 
     def start(self):
-        self.print_instructions()
-        while True:
-            print("*" * 20)
-            print("Calculator: " + str(self.c))
-            user_input = input("> ")
-            if user_input == "q":
-                break
-            else:
-                result = self.c.input(user_input)
-                if "error" in result.lower():
-                    print(result)
+        try:
+            self.print_instructions()
+            while True:
+                print("*" * 20)
+                print("Calculator: " + str(self.c))
+                user_input = input("> ")
+                if user_input == "q":
+                    raise Shutdown
+                else:
+                    result = self.c.input(user_input)
+                    if "error" in result.lower():
+                        print(result)
+        except Shutdown:
+            self.stop("User initiated shutdown.  Exiting.")
+        except EOFError:
+            self.stop("\nReceived end of file.  Exiting.")
+        except KeyboardInterrupt:
+            self.stop("\nReceived keyboard interrupt.  Exiting.")
+        except:
+            self.stop("Unexpected error occurred. Exiting.")
 
-    def stop(self):
-        print("To be implemented")
+    def stop(self, message):
+        print(message)
+        exit()
 
     def print_instructions(self):
         print("Welcome to the command line RPN Calculator!")
